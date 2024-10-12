7130649f2dc47a1635adf40010881aac14571f3b
«movavok» <pkurishchenkoukr.net05@gmail.com>
2024-10-12 20:52:59 +0300

    Revert "Перевод через git"
    This reverts commit 178edfb804f949bd7ded52bc83150458fafff609.

--- Lesson_1/Task_3/Calculator.cpp
+++ Lesson_1/Task_3/Calculator.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -29,36 +28,4 @@ int main() {
     cout << "The result is " << res << endl;
 
     return 0;
-=======
-#include <iostream>
-using namespace std;
-
-int main() {
-    double a, b, res;
-    char oper;
-    cout<< "Enter first number   ";
-    cin >> a;
-    cout<< "Enter second number   ";
-    cin >> b;
-    cout<< "Enter math operation( + , - , * , / )   ";
-    cin >> oper;
-    switch(oper){
-        case '+':
-            res = a + b;
-            break;
-        case '-':
-            res = a - b;
-            break;
-        case '*':
-            res = a * b;
-            break;
-        case '/':
-            res = a / b;
-            break;
-    }
-    
-    cout << "The result is " << res << endl;
-
-    return 0;
->>>>>>> parent of 7df52b4 (Delete Lesson_1 directory)
 }
--- Lesson_1/Task_4/Millionaire.cpp
+++ Lesson_1/Task_4/Millionaire.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -30,37 +29,4 @@ int main() {
     }
 
     return 0;
-=======
-#include <iostream>
-using namespace std;
-
-int main() {
-    
-    string earn;
-    cout << "Если не секрет, напиши (число) сколько ты зарабатываешь?";
-    cin >> earn;
-    int len = earn.length();
-    
-    switch(len){
-        case(1):
-        case(2):
-        case(3):
-            cout << "Нужно больше работать!!!";
-            break;
-        case(4):
-        case(5):
-        case(6):
-            cout << "Красавец";
-            break;
-        case(7):
-        case(8):
-        case(9):
-            cout << "Ты миллионер<3";
-            break;
-        default:
-            cout << "Миллиардер??";
-    }
-
-    return 0;
->>>>>>> parent of 7df52b4 (Delete Lesson_1 directory)
 }
