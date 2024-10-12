178edfb804f949bd7ded52bc83150458fafff609
«movavok» <pkurishchenkoukr.net05@gmail.com>
2024-10-12 20:51:10 +0300

    Перевод через git

--- Lesson_1/Task_2/My name.cpp
+++ Lesson_1/Task_2/My name.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -6,4 +7,14 @@ int main() {
     cout << "Pavlo\nKurishchenko" << endl;
 
     return 0;
+=======
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Pavlo\nKurishchenko" << endl;
+
+    return 0;
+>>>>>>> parent of 7df52b4 (Delete Lesson_1 directory)
 }
