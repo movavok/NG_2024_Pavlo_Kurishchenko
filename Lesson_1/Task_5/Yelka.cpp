8271b5c24ee9e23ea975cc333b71e0edcffe63f3
«movavok» <pkurishchenkoukr.net05@gmail.com>
2024-10-19 18:14:20 +0300

    Task_5 rework

--- Lesson_1/Task_5/Yelka.cpp
+++ Lesson_1/Task_5/Yelka.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 int main() {
-    int h;
-    cout << "Введи высоту елочки (целое число) - ";
-    cin >> h;
+    
+    int hgt; // переменная не в 1 символ)
+    cout << "Введи высоту елочки (целое число > 2) - ";
+    cin >> hgt;
+    
+    if (hgt <= 2){ 
+        
+        cout << "Введи положительное целое число > 2";
+        return 1;
+        
+    }
+    
     int nstar = 1;
-    int nspac = h - 1;
+    int nspace = hgt - 1;
     
+    while(hgt > 0, hgt--){ // скоротил
+    for (int i = 0; i < nspace; i++) cout << " ";
+    for (int i = 0; i < nstar; i++) cout << "*";
     
-    while(h > 0){
-    for (int i = 0; i < nspac; i++){
-        cout << " ";
-    }
-    for (int i = 0; i < nstar; i++){
-        cout << "*";
-    }
+    nspace--;
+    nstar += 2;
     cout << endl;
-    switch(nspac){
-        case(0):
-        for (int i = 0; i < (nstar - 1) / 2; i++){
-            cout << " ";
-        }
-        cout << "*";
     }
-    nspac = nspac - 1;
-    nstar = nstar + 2;
-    h = h - 1;
-    }
+    
+    for (int i = 1; i < (nstar - 1) / 2; i++) cout << " "; // вывел пенек с цыкла
+    cout << "*";
 
     return 0;
 
