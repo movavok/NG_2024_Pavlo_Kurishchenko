e6402c7f192ae052eb1958f562f32ab2b85e6bda
«movavok» <pkurishchenkoukr.net05@gmail.com>
2024-10-23 23:43:24 +0300

    Lesson_1\Task_3 reworked

--- Lesson_1/Task_3/Calculator.cpp
+++ Lesson_1/Task_3/Calculator.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 
 int main() {
-    double num1, num2, res; // Поменял переменные а и б на ном1, ном2
+    
+    double num1, num2, res; 
     char oper;
-    cout<< "Enter first number   ";
+    
+    cout<< "Enter two numbers and an operation to calculate a formula like (number1 operation number2 = result)\n";
+    
+    cout<< "Enter first number >> ";
     cin >> num1;
-    cout<< "Enter second number   ";
+    
+    cout<< "Enter second number >> ";
     cin >> num2;
-    cout<< "Enter math operation( + , - , * , / )   ";
+    
+    cout<< "Enter math operation( + or - or * or / ) >> ";
     cin >> oper;
+    
     switch(oper){
         case '+':
             res = num1 + num2;
